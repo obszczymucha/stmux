2a24a7d5d8065d190e7125e78a34a6f910e2218e
Obszczymucha <obszczymucha@gmail.com>
2024-09-29 20:02:42 +1000

    Merge sessions when saving.

--- TODO.md
+++
@@ -1,4 +0,0 @@
-# TODO
-
-1. Make the configuration safe. Instead of overwriting the config, read it and only add non-existing entries.
-
--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,6 @@ use clap::Parser;
 use dirs_next::home_dir;
 use model::TmuxSession;
 use model::TmuxWindow;
-use utils::is_numeric;
 use std::collections::HashMap;
 use std::fs;
 use tmux::get_tmux_sessions;
@@ -16,6 +15,7 @@ use tmux::has_session;
 use tmux::new_session;
 use tmux::new_window;
 use tmux::select_window;
+use utils::is_numeric;
 
 const CONFIG_LOCATION: &str = ".config/stmux";
 const DEFAULT_FILENAME: &str = "sessions.toml";
@@ -51,20 +51,48 @@ fn main() {
     }
 }
 
+fn load_sessions_from_config(filename: &str) -> HashMap<String, TmuxSession> {
+    let file_content = fs::read_to_string(filename);
+    match file_content {
+        Ok(content) => {
+            toml::from_str(&content).unwrap_or_else(|_| panic!("Failed to parse {}.", filename))
+        }
+        Err(_) => HashMap::new(),
+    }
+}
+
+fn merge(
+    old_sessions: HashMap<String, TmuxSession>,
+    new_sessions: HashMap<String, TmuxSession>,
+) -> HashMap<String, TmuxSession> {
+    let mut sessions = HashMap::new();
+
+    for (session_name, session) in old_sessions {
+        sessions.insert(session_name, session);
+    }
+
+    for (session_name, session) in new_sessions {
+        if !sessions.contains_key(session_name.as_str()) {
+            sessions.insert(session_name, session);
+        }
+    }
+
+    sessions
+}
+
 fn save_sessions(filename: &str) {
-    let sessions = get_tmux_sessions();
+    let old_sessions = load_sessions_from_config(filename);
+    let new_sessions = get_tmux_sessions();
+    let sessions = merge(old_sessions, new_sessions);
     let toml_string = toml::to_string(&sessions).expect("Failed to serialize sessions into TOML.");
+
     fs::write(filename, toml_string).unwrap_or_else(|_| panic!("Failed to write to {}.", filename));
     eprintln!("TMUX sessions saved to {} file.", filename);
 }
 
 fn restore_sessions(filename: &str) {
-    let file_content = fs::read_to_string(filename)
-        .unwrap_or_else(|_| panic!("Failed to read {} file.", filename));
-    let sessions: HashMap<String, TmuxSession> =
-        toml::from_str(&file_content).unwrap_or_else(|_| panic!("Failed to parse {}.", filename));
-
     eprintln!("Restoring TMUX sessions from {} file...", filename);
+    let sessions = load_sessions_from_config(filename);
 
     for (session_name, session) in sessions {
         let windows = session.windows;
