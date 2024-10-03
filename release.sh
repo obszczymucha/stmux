9238fa14f2588072797744156ecfe0a5ac4698ee
Obszczymucha <obszczymucha@gmail.com>
2024-10-03 19:16:21 +1000

    Copy neovim config in release.sh.

--- release.sh
+++ release.sh
@@ -1,1 +1,9 @@
-cargo build --release && sudo cp target/release/stmux /usr/local/bin
+#!/usr/bin/env bash
+
+function main() {
+  cp config/nvim-config.lua "$HOME/.config/stmux/"
+  cargo build --release && sudo cp target/release/stmux /usr/local/bin
+}
+
+main "$@"
+
