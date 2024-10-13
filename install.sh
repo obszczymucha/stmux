fc379fe36ed0b6590548c372c92c6d5daf7b77a0
Obszczymucha <obszczymucha@gmail.com>
2024-10-14 08:43:41 +1100

    Parametrize user and user's home in install.sh.

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
 function main() {
-  cargo build --release && cp config/nvim-config.lua "/home/alien/.config/stmux/" && cp target/release/stmux /usr/local/bin && su - alien -c 'tmux display-message "#[fg=#8a60ab,align=centre]stmux #[fg=#e0e0e0]ready"'
+  cargo build --release && cp config/nvim-config.lua "${TMUX_USER_HOME}/.config/stmux/" && cp target/release/stmux /usr/local/bin && su - "$TMUX_USER" -c 'tmux display-message "#[fg=#8a60ab,align=centre]stmux #[fg=#e0e0e0]ready"'
 }
 
 main "$@"
