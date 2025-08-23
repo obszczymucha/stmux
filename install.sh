7bf3c30558cdf33c4e7a42c405d4bcce336a5408
Obszczymucha <obszczymucha@gmail.com>
2025-08-23 11:31:46 +1000

    Remove stupid environment variables from install.sh.

--- install.sh
+++ install.sh
@@ -2,7 +2,9 @@
 set -euo pipefail
 
 function main() {
-  cargo build --release && cp config/nvim-config.lua "${TMUX_USER_HOME}/.config/stmux/" && sudo cp target/release/stmux /usr/local/bin && su - "$TMUX_USER" -c 'tmux display-message "#[fg=#8a60ab,align=centre]stmux #[fg=#e0e0e0]ready"'
+  local user="$USER"
+  local user_home="$HOME"
+  cargo build --release && cp config/nvim-config.lua "${user_home}/.config/stmux/" && sudo cp target/release/stmux /usr/local/bin && su - "$user" -c 'tmux display-message "#[fg=#8a60ab,align=centre]stmux #[fg=#e0e0e0]ready"'
 }
 
 main "$@"
