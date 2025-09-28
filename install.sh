5d0e2c1ec642c75230980a10f24cb7ea17b80dbb
Obszczymucha <obszczymucha@gmail.com>
2025-09-28 10:14:01 +1000

    Create stmux config directory if it doesn't exist in install.sh.

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 function main() {
   local user="$USER"
   local user_home="$HOME"
+  mkdir -p "$HOME/.config/stmux"
   cargo build --release && cp config/nvim-config.lua "${user_home}/.config/stmux/" && sudo cp target/release/stmux /usr/local/bin && su - "$user" -c 'tmux display-message "#[fg=#8a60ab,align=centre]stmux #[fg=#e0e0e0]ready"'
 }
 
